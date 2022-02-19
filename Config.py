a3bc548c104640d25450afd309100e14aae714d9
baquanluomu <65743350+baquanluomu@users.noreply.github.com>
2022-02-19 14:29:12 +0800

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,16 @@
+config = {
+    # 网站标题
+    # 来自魂炎改版
+    "Title": "www.luomubiji.host",
+    # 回收时间分钟 不填默认3分钟
+    "Closetime": 5,
+    # 网站公告
+    "Announcement": "为提高账户的安全性，请关闭免密支付。",
+    # XDD PLUS Url  http://IP地址:端口/api/login/smslogin
+    # 若XDDurl留空，则不对接任何东西，CK显示，理论可用傻妞对接
+    "XDDurl": "",
+    # xddToken
+    "XDDToken": "",
+    #量子公共服务平台 http://IP:端口/api/sms/   后面斜杠不要忘
+    "LZGZ":"",
+}
